6dc9eeb00b976bd1c33910397cd4e0f4c7f5bde8
AmitrajitDas <amitrajitdas31@gmail.com>
2022-07-31 15:30:14 +0530

    setup: screens and routing

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react"
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View, Button } from "react-native"
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+
+import LoginScreen from "./pages/LoginScreen"
+import SignupScreen from "./pages/SignupScreen"
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
+  const [auth, setAuth] = useState()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#0096FF",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            headerRight: () => (
+              <Button
+                onPress={() => alert("This is a button!")}
+                title='Info'
+                color='#000'
+                style={styles.navBtn}
+              />
+            ),
+          }}
+          initialRouteName='Login'
+        >
+          <Stack.Screen
+            name='Login'
+            component={LoginScreen}
+            options={{ title: "Login" }}
+          />
+          <Stack.Screen
+            name='Signup'
+            component={SignupScreen}
+            options={{ title: "Signup" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  navBtn: {
+    borderRadius: "50%",
   },
-});
+})
