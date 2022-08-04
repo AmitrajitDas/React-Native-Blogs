470aab2053796a355332eb297a58547197081923
AmitrajitDas <amitrajitdas31@gmail.com>
2022-08-04 09:46:18 +0530

    feat: user blog screen

---
+++ pages/MyBlogScreen/index.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react"
+import { StatusBar } from "expo-status-bar"
+import {
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Image,
+  AsyncStorage,
+} from "react-native"
+import api from "../../api"
+import Card from "../../components/Card"
+import { styles } from "./styles"
+
+const MyBlogScreen = ({ navigation }) => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      setLoading(true)
+      try {
+        const { data } = await api.get("/blogs")
+        console.log(data)
+        const userIdString = await AsyncStorage.getItem("userId")
+        const userId = JSON.parse(userIdString)
+        await setBlogs(data.filter((x) => x.user_id === userId))
+      } catch (error) {
+        console.log(error.response.data.error)
+        setError(error.response.data.error)
+        alert(error.response.data.error)
+      }
+      setLoading(false)
+    }
+    fetchBlogs()
+  }, [])
+
+  return (
+    <View>
+      <ScrollView style={styles.container}>
+        {blogs &&
+          blogs.map((blog) => (
+            <Card key={blog.id} blog={blog} navigation={navigation} />
+          ))}
+      </ScrollView>
+      <Text>MyBlogScreen</Text>
+    </View>
+  )
+}
+
+export default MyBlogScreen
